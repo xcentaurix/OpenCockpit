37e9dccbc432758499d644df3ed500d358c7cab5
dream-alpha <swmaniacster@gmail.com>
2025-11-27 19:35:18 +0100

    Open

--- src/Version.py
+++ src/Version.py
@@ -3,8 +3,8 @@
 # License: GNU General Public License v3.0 (see LICENSE file for details)
 
 
-PLUGIN = "DreamCockpit"
+PLUGIN = "OpenCockpit"
 ID = "CPT"
-VERSION = "0.1.0"
+VERSION = "0.0.0"
 COPYRIGHT = "2018-2025 by dream-alpha"
 LICENSE = "This program is free software: you can redistribute it and/or modify it under the terms of the GNU General Public License as published by the Free Software Foundation, either version 3 of the License, or (at your option) any later version."
